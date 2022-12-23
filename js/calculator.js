c1962b39e3f0d54defe5e96950757e2451eadd6f
marvall <marvallua@gmail.com>
2022-12-23 13:34:54 +0200

    update

--- js/calculator.js
+++ js/calculator.js
@@ -10,134 +10,242 @@
  * Написати коментарі до функцій.
  */
 
-/**
- * Ініціалізую роботу калькулятора.
- */
-const main = function () {
-  const userInput = prompt(
-    'Введіть будь ласка операнди та дію між ними у форматі "2 + 4"'
-  );
+// /**
+//  * Ініціалізую роботу калькулятора.
+//  */
+// const main = function () {
+//   const userInput = prompt(
+//     'Введіть будь ласка операнди та дію між ними у форматі "2 + 4"'
+//   );
 
-  const operands = parseString(userInput);
-  if (operands) {
-    const operation = checkOperation(operands[1]);
-    const result = operation(Number(operands[0]), Number(operands[2]));
-    alert("Результат: " + result);
-  } else {
-    sendErrorMessage();
-  }
-};
+//   const operands = parseString(userInput);
+//   if (operands) {
+//     const operation = checkOperation(operands[1]);
+//     const result = operation(Number(operands[0]), Number(operands[2]));
+//     alert("Результат: " + result);
+//   } else {
+//     sendErrorMessage();
+//   }
+// };
 
-/**
- * Перевіряє що 1 і 3 елементи дорівнють числу.
- * А 2 елемнт дорівнює дії.
- * @param {Array} array
- * @returns {Boolean} Резут превірки.
- */
-const checkArrayElements = function (array) {
-  let result = true;
-  const operaion = ["+", "-", "*", "/", "%"];
+// /**
+//  * Перевіряє що 1 і 3 елементи дорівнють числу.
+//  * А 2 елемнт дорівнює дії.
+//  * @param {Array} array
+//  * @returns {Boolean} Резут превірки.
+//  */
+// const checkArrayElements = function (array) {
+//   let result = true;
+//   const operaion = ["+", "-", "*", "/", "%"];
 
-  if (!Number(array[0]) || !Number(array[2])) {
-    result = false;
-  }
+//   if (!Number(array[0]) || !Number(array[2])) {
+//     result = false;
+//   }
 
-  if (!operaion.includes(array[1])) {
-    result = false;
+//   if (!operaion.includes(array[1])) {
+//     result = false;
+//   }
+
+//   return result;
+// };
+
+// /**
+//  * Розбыває строку на масив.
+//  * Повертає массив, якщо той відповідає перевіркі checkArrayElements
+//  * @param {String} string
+//  * @returns {Array || Boolean}
+//  */
+// const parseString = function (string) {
+//   const parsedStrinToArray = string.split(" ");
+
+//   return checkArrayElements(parsedStrinToArray) && parsedStrinToArray;
+// };
+
+// /**
+//  * Додає одне число до іншого.
+//  * @param {Number} number1
+//  * @param {Number} number2
+//  * @returns {Number} результат дії дадавання
+//  */
+// const summ = function (number1, number2) {
+//   return number1 + number2;
+// };
+
+// /**
+//  * Віднімає одне число від іншого.
+//  * @param {Number} number1
+//  * @param {Number} number2
+//  * @returns {Number} результат дії віднімання
+//  */
+// const subtraction = function (number1, number2) {
+//   return number1 - number2;
+// };
+
+// /**
+//  * Множить одне число на інше.
+//  * @param {Number} number1
+//  * @param {Number} number2
+//  * @returns {Number} результат дії множення
+//  */
+// const multiplication = function (number1, number2) {
+//   return number1 * number2;
+// };
+
+// /**
+//  * Ділить одне число на інше.
+//  * @param {Number} number1
+//  * @param {Number} number2
+//  * @returns {Number} результат дії ділення
+//  */
+// const division = function (number1, number2) {
+//   return number1 / number2;
+// };
+
+// /**
+//  * Розраховує відсоток одного числа від іншого.
+//  * @param {Number} number1
+//  * @param {Number} number2
+//  * @returns {Number} результат дії процент
+//  */
+// const percent = function (number1, number2) {
+//   return (number2 / 100) * number1;
+// };
+
+// /**
+//  * Повертає математичну операцію.
+//  * @param {String} operation
+//  * @returns {Function} Математична операція.
+//  */
+// const checkOperation = function (operation) {
+//   switch (operation) {
+//     case "+":
+//       return summ;
+//     case "-":
+//       return subtraction;
+//     case "*":
+//       return multiplication;
+//     case "/":
+//       return division;
+//     case "%":
+//       return percent;
+//     default:
+//       break;
+//   }
+// };
+
+// /**
+//  * Виводить повідомлення про не вірний формат данних
+//  */
+// const sendErrorMessage = function () {
+//   alert("Не вірний формат введених данних");
+// };
+
+// main();
+
+//=========================================================
+// =======================================================
+// ========================================================
+
+function checkOperators(array) {
+  let result = false;
+  const operation = ['+', '-', '*', '/', '%'];
+
+  if (
+    array.length === 3 &&
+    Number(array[0]) &&
+    Number(array[2]) &&
+    operation.includes(array[1])
+  ) {
+    result = true;
   }
 
   return result;
-};
+}
 
-/**
- * Розбыває строку на масив.
- * Повертає массив, якщо той відповідає перевіркі checkArrayElements
- * @param {String} string
- * @returns {Array || Boolean}
- */
-const parseString = function (string) {
-  const parsedStrinToArray = string.split(" ");
+function getOperators(string) {
+  const result = string.split(' ');
+  return (
+    checkOperators(result) && [Number(result[0]), result[1], Number(result[2])]
+  );
+}
 
-  return checkArrayElements(parsedStrinToArray) && parsedStrinToArray;
-};
-
-/**
- * Додає одне число до іншого.
- * @param {Number} number1
- * @param {Number} number2
- * @returns {Number} результат дії дадавання
- */
-const summ = function (number1, number2) {
-  return number1 + number2;
-};
-
-/**
- * Віднімає одне число від іншого.
- * @param {Number} number1
- * @param {Number} number2
- * @returns {Number} результат дії віднімання
- */
-const subtraction = function (number1, number2) {
+function summ(number1, number2) {
+  const result = number1 + number2;
+  return result;
+}
+function subtraction(number1, number2) {
   return number1 - number2;
-};
-
-/**
- * Множить одне число на інше.
- * @param {Number} number1
- * @param {Number} number2
- * @returns {Number} результат дії множення
- */
-const multiplication = function (number1, number2) {
+}
+function multiplication(number1, number2) {
   return number1 * number2;
-};
+}
 
 /**
- * Ділить одне число на інше.
- * @param {Number} number1
- * @param {Number} number2
- * @returns {Number} результат дії ділення
+ *
+ * @param {*} number1
+ * @param {*} number2
+ * @returns
  */
-const division = function (number1, number2) {
+function division(number1, number2) {
   return number1 / number2;
-};
+}
 
 /**
- * Розраховує відсоток одного числа від іншого.
- * @param {Number} number1
- * @param {Number} number2
- * @returns {Number} результат дії процент
+ *
+ * @param {*} number1
+ * @param {*} number2
+ * @returns
  */
-const percent = function (number1, number2) {
-  return (number2 / 100) * number1;
-};
+function percent(number1, number2) {
+  return (number1 / 100) * number2;
+}
 
 /**
- * Повертає математичну операцію.
+ * Find math operation
  * @param {String} operation
- * @returns {Function} Математична операція.
+ * @returns {function} Math operation
  */
-const checkOperation = function (operation) {
+function getOperation(operation) {
   switch (operation) {
-    case "+":
+    case '+':
       return summ;
-    case "-":
+    case '-':
       return subtraction;
-    case "*":
+    case '*':
       return multiplication;
-    case "/":
+    case '/':
       return division;
-    case "%":
+    case '%':
       return percent;
-    default:
-      break;
   }
-};
+}
 
 /**
- * Виводить повідомлення про не вірний формат данних
+ * initializes the program
  */
-const sendErrorMessage = function () {
-  alert("Не вірний формат введених данних");
-};
+function main() {
+  const prmt = prompt(
+    "Введіть числа та операцію в форматі 'число' 'операція' 'число'"
+  );
+  const operators = getOperators(prmt);
+  if (operators) {
+    const operation = getOperation(operators[1]);
+    const message = operation(operators[0], operators[2]);
+    alert(message);
+  } else {
+    alert("Невірний формат! Потрібно ввести 'число' 'операція' 'число'");
+  }
+}
 
 main();
+
+// 1. написати маін. викликати її
+// 2. В середі маін отримати дані від юзера.
+// 3. Умовно формат данних це массив [число, дія, число]
+// 4. перевірити що в моему масиві данні відповідні
+//   4.1. написати функції яка приймає масив
+//   4.2 перевірити що 1й і останній елемент - це числа
+//   4.3 перевырити, що 2й елемент відповідає допустимим операціям
+// 5. придумати логіку по якій я може викликати якусь мат.операції відповідну до дії
+// 6. кудись зберегти результат роботи
+// 7. Вивести результат роботи
--- js/shop.js
+++ js/shop.js
@@ -7,7 +7,7 @@
  * При цьому якщо товар з таким ім'ям та категорією вже існую, /
  * То замість того щоб добавляти об'єкт торару збільшите кількість (amount) в існуючому обьекті.
  *
- * 2. Створити метод котрый приймає в себе категорію або ім'я товару (я
+ * 2. Створити метод котрый приймає в себе категорію або ім'я товару (
  * Фільтрує наш масив на складі та повертає нам або список товарів по категорії, або об'єкт конкретного товар.
  *
  * Дотатково (як буде час)
@@ -17,187 +17,251 @@
 
 const initData = [
   {
-    name: "SmartPhone S20",
-    category: "tech",
+    name: 'SmartPhone S20',
+    category: 'tech',
     price: 1000,
-    qualit: "hight",
+    qualit: 'hight',
   },
   {
-    name: "SmartPhone S20",
-    category: "tech",
+    name: 'SmartPhone S20',
+    category: 'tech',
     price: 1000,
-    qualit: "hight",
+    qualit: 'hight',
   },
   {
-    name: "NoteBook K11",
-    category: "tech",
+    name: 'NoteBook K11',
+    category: 'tech',
     price: 1500,
-    qualit: "hight",
+    qualit: 'hight',
   },
   {
-    name: "SmartPhone G5",
-    category: "tech",
+    name: 'SmartPhone G5',
+    category: 'tech',
     price: 700,
-    qualit: "low",
+    qualit: 'low',
   },
   {
-    name: "SmartPhone G5",
-    category: "tech",
+    name: 'SmartPhone G5',
+    category: 'tech',
     price: 700,
-    qualit: "low",
+    qualit: 'low',
   },
   {
-    name: "SmartPhone G5",
-    category: "tech",
+    name: 'SmartPhone G5',
+    category: 'tech',
     price: 700,
-    qualit: "low",
+    qualit: 'low',
   },
   {
-    name: "Lamp G1",
-    category: "house",
+    name: 'Lamp G1',
+    category: 'house',
     price: 10,
-    qualit: "low",
+    qualit: 'low',
   },
   {
-    name: "Lamp G1",
-    category: "house",
+    name: 'Lamp G1',
+    category: 'house',
     price: 10,
-    qualit: "low",
+    qualit: 'low',
   },
   {
-    name: "Lamp G1",
-    category: "house",
+    name: 'Lamp G1',
+    category: 'house',
     price: 10,
-    qualit: "low",
+    qualit: 'low',
   },
   {
-    name: "Frame",
-    category: "house",
+    name: 'Frame',
+    category: 'house',
     price: 2,
-    qualit: "low",
+    qualit: 'low',
   },
   {
-    name: "Vase Silver C",
-    category: "house",
+    name: 'Vase Silver C',
+    category: 'house',
     price: 71,
-    qualit: "hight",
+    qualit: 'hight',
   },
   {
-    name: "tab S20",
-    category: "electronic",
+    name: 'tab S20',
+    category: 'electronic',
     price: 1500,
-    qualit: "low",
+    qualit: 'low',
   },
   {
-    name: "tab S20",
-    category: "electronic",
-    price: 1500,
-    qualit: "low",
+    name: 'SmartPhone S20',
+    category: 'tech',
+    price: 1000,
+    qualit: 'hight',
   },
   {
-    name: "tab S20",
-    category: "electronic",
+    name: 'tab S20',
+    category: 'electronic',
     price: 1500,
-    qualit: "low",
+    qualit: 'low',
   },
   {
-    name: "tab S20",
-    category: "electronic",
+    name: 'tab S20',
+    category: 'electronic',
     price: 1500,
-    qualit: "low",
+    qualit: 'low',
+  },
+  {
+    name: 'tab S20',
+    category: 'electronic',
+    price: 1500,
+    qualit: 'low',
   },
 ];
 // console.log(initData);
-function main() {
-  initData.forEach((item) => shop.addItemsToWarehouse(item));
-  // console.log(shop.getItemsFromWarehouse());
-  // console.log(shop.filterItemFromWarehouse('tech'));
-  // console.log(shop.filterItemFromWarehouse('house'));
-  // console.log(shop.filterItemFromWarehouse('NoteBook K11'));
-  // console.log(shop.filterItemFromWarehouse('SmartPhone S20'));
-  // console.log(shop.filterItemFromWarehouse('Lamp G1'));
-  // console.log(shop.filterItemFromWarehouse('NoteBook K3'));
+// function main() {
+//   initData.forEach((item) => shop.addItemsToWarehouse(item));
+//   // console.log(shop.getItemsFromWarehouse());
+//   // console.log(shop.filterItemFromWarehouse('tech'));
+//   // console.log(shop.filterItemFromWarehouse('house'));
+//   // console.log(shop.filterItemFromWarehouse('NoteBook K11'));
+//   // console.log(shop.filterItemFromWarehouse('SmartPhone S20'));
+//   // console.log(shop.filterItemFromWarehouse('Lamp G1'));
+//   // console.log(shop.filterItemFromWarehouse('NoteBook K3'));
 
-  // shop.changeItemNameFromWarehouse(shop.getRandomItemId(), 'Abrekadabra');
-  // shop.changeAnyFieldItemFromWarehouse(shop.getRandomItemId(), 'qualit', 'A');
-  // shop.changeAnyFieldItemFromWarehouse(shop.getRandomItemId(), 'price', 10000);
-  // console.log(shop.getItemsFromWarehouse());
+//   // shop.changeItemNameFromWarehouse(shop.getRandomItemId(), 'Abrekadabra');
+//   // shop.changeAnyFieldItemFromWarehouse(shop.getRandomItemId(), 'qualit', 'A');
+//   // shop.changeAnyFieldItemFromWarehouse(shop.getRandomItemId(), 'price', 10000);
+//   // console.log(shop.getItemsFromWarehouse());
 
-  console.log(shop.getRandomItemId());
-}
+//   console.log(shop.getRandomItemId());
+// }
+// const shop = {
+//   warehouse: [],
+//   getItemsFromWarehouse() {
+//     return this.warehouse;
+//   },
+//   getRandomItemId() {
+//     // const warehouseLength = this.warehouse.length; // max
+//     // const randomFloat = Math.random() * (warehouseLength - 0) + 0;
+//     // const randomIndex = Math.floor(randomFloat);
+//     // const itemByIndex = this.warehouse[randomIndex];
+
+//     // console.log('warehouseLength: ', warehouseLength);
+//     // console.log('randomFloat: ', randomFloat);
+//     // console.log('randomIndex: ', randomIndex);
+//     // console.log('itemByIndex: ', itemByIndex);
+//     // return this.warehouse[randomIndex].id;
+
+//     return this.warehouse[
+//       Math.floor(Math.random() * (this.warehouse.length - 0) + 0)
+//     ].id;
+//   },
+//   // changeItemNameFromWarehouse(id, name) {
+//   //   for (const elem of this.warehouse) {
+//   //     if (elem.id === id) {
+//   //       elem.name = name;
+//   //       break;
+//   //     }
+//   //   }
+//   // },
+//   filterItemFromWarehouse(filter) {
+//     const filteredArray = [];
+//     let foundItem = false;
+//     for (const elem of this.warehouse) {
+//       if (elem.name === filter) {
+//         foundItem = elem;
+//         break;
+//       }
+//       if (elem.category === filter) {
+//         filteredArray.push(elem);
+//       }
+//     }
+
+//     return foundItem
+//       ? foundItem
+//       : filteredArray.length
+//       ? filteredArray
+//       : "Items not found";
+//   },
+
+//   addItemsToWarehouse(item) {
+//     let foundItemFromWarehouse = false;
+//     for (const elem of this.warehouse) {
+//       if (elem.name === item.name) {
+//         foundItemFromWarehouse = elem;
+//       }
+//     }
+//     if (!foundItemFromWarehouse) {
+//       const newItem = { ...item };
+//       newItem.id = Math.floor(Math.random() * 1000 ** 5).toString(36);
+//       newItem.amount = 1;
+//       this.warehouse.push(newItem);
+//     } else {
+//       foundItemFromWarehouse.amount += 1;
+//     }
+//   },
+
+//   changeAnyFieldItemFromWarehouse(id, key, value) {
+//     for (const elem of this.warehouse) {
+//       if (elem.id === id) {
+//         elem[key] = value;
+//         break;
+//       }
+//     }
+//   },
+// };
+// main();
+
+// {
+//   name: 'tab S20',
+//   category: 'electronic',
+//   price: 1500,
+//   qualit: 'low',
+// },
+
 const shop = {
   warehouse: [],
-  getItemsFromWarehouse() {
-    return this.warehouse;
-  },
-  getRandomItemId() {
-    // const warehouseLength = this.warehouse.length; // max
-    // const randomFloat = Math.random() * (warehouseLength - 0) + 0;
-    // const randomIndex = Math.floor(randomFloat);
-    // const itemByIndex = this.warehouse[randomIndex];
 
-    // console.log('warehouseLength: ', warehouseLength);
-    // console.log('randomFloat: ', randomFloat);
-    // console.log('randomIndex: ', randomIndex);
-    // console.log('itemByIndex: ', itemByIndex);
-    // return this.warehouse[randomIndex].id;
-
-    return this.warehouse[
-      Math.floor(Math.random() * (this.warehouse.length - 0) + 0)
-    ].id;
-  },
-  // changeItemNameFromWarehouse(id, name) {
-  //   for (const elem of this.warehouse) {
-  //     if (elem.id === id) {
-  //       elem.name = name;
-  //       break;
-  //     }
-  //   }
-  // },
-  filterItemFromWarehouse(filter) {
-    const filteredArray = [];
-    let foundItem = false;
-    for (const elem of this.warehouse) {
-      if (elem.name === filter) {
-        foundItem = elem;
+  addItemToWarehouse(product) {
+    // 1. перевірити, чи є в складі вже такий товар.
+    // 2. збыльшити його кількість
+    // 3. Додати цей товар товар до warehouse, створити йому id & amount = 1
+    let currentProduct;
+    for (const item of this.warehouse) {
+      if (item && product.name === item.name) {
+        currentProduct = item;
         break;
       }
-      if (elem.category === filter) {
-        filteredArray.push(elem);
-      }
     }
-
-    return foundItem
-      ? foundItem
-      : filteredArray.length
-      ? filteredArray
-      : "Items not found";
-  },
-
-  addItemsToWarehouse(item) {
-    let foundItemFromWarehouse = false;
-    for (const elem of this.warehouse) {
-      if (elem.name === item.name) {
-        foundItemFromWarehouse = elem;
-      }
-    }
-    if (!foundItemFromWarehouse) {
-      const newItem = { ...item };
-      newItem.id = Math.floor(Math.random() * 1000 ** 5).toString(36);
-      newItem.amount = 1;
-      this.warehouse.push(newItem);
+    // console.log('CURRENT PRODUCT: ', currentProduct);
+    if (currentProduct) {
+      // console.log('IS');
+      currentProduct.amount += 1;
     } else {
-      foundItemFromWarehouse.amount += 1;
-    }
-  },
+      // console.log('NO');
+      console.log('step 1:', Math.random());
+      console.log('step 2:', Math.random() * 100000000000);
+      console.log('step 3:', Math.floor(Math.random() * 100000000000));
+      console.log(
+        'step 4:',
+        Math.floor(Math.random() * 100000000000).toString(36)
+      );
+      const id = Math.floor(Math.random() * 100000000000).toString(36);
 
-  changeAnyFieldItemFromWarehouse(id, key, value) {
-    for (const elem of this.warehouse) {
-      if (elem.id === id) {
-        elem[key] = value;
-        break;
-      }
+      const newObject = {
+        id,
+        ...product,
+        amount: 1,
+      };
+      // console.log('NEW OBJECT: ', newObject);
+      this.warehouse.push(newObject);
     }
   },
 };
+
+function main() {
+  for (const item of initData) {
+    // console.group('ITEM IN MAIN: ', item);
+    shop.addItemToWarehouse(item);
+    // console.log(shop.warehouse);
+    // console.groupEnd();
+  }
+}
+
 main();
