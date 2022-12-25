68ab417800ed057e7f6ee0b2e1303ec97d1f6377
marvall (Yaroslav Bezugliy) <marvallua@gmail.com>
2022-12-25 23:13:02 +0200

    init

--- js/index.js
+++ js/index.js
@@ -1,434 +1,373 @@
-/**
- * Example 4
- * Перебір об'ектов: for...in та методи Object.keys|values|entries|hasOwnProperty
- */
-
-// const obj = {
-//   name: "Bob",
-//   age: 25,
-//   games: ["footbal", "tenis"],
-//   isWork: true,
-//   sayHello: function () {
-//     console.log("Hello");
-//   },
-//   contacts: {
-//     email: "example@domain.com",
-//     phone: "8800-000-00-99",
-//   },
-// };
-
-// for (const key in obj) {
-//   //obj["name"]
-//   //obj["age"]
-//   console.log(obj[key]);
-// }
-
-// for (const key of Object.keys(obj)) {
-//   console.log(key, ": ", obj[key]);
-// }
-
-// for (const value of Object.values(obj)) {
-//   console.log(value);
-// }
-// for (const [index, value] of Object.entries(obj)) {
-//   console.log("index: ", index);
-//   console.log("value: ", value);
-// }
-
-// console.log(Object.keys(obj));
-// console.log(Object.values(obj));
-// console.log(Object.entries(obj));
-
-// console.log(obj);
-// console.log(obj.hasOwnProperty("games"));
-
-// console.log(obj);
-/**
- * Example 5
- * Методи: freeze, defineProperty
- */
-
-// const obj = {
-//   name: "Bob",
-//   age: 25,
-//   games: ["footbal", "tenis"],
-//   isWork: true,
-//   sayHello: function () {
-//     console.log("Hello");
-//   },
-//   contacts: {
-//     email: "example@domain.com",
-//     phone: "8800-000-00-99",
-//   },
-// };
-
-// Object.freeze(obj);
-
-// obj.name = 'James';
-// console.log(obj);
-
-// Object.defineProperty(obj, 'name', {
-//   enumerable: false,
-//   configurable: false,
-//   writable: false,
-//   //   value: 'static',
-// });
-
-// Object.defineProperty(obj, 'name', {
-//   enumerable: true,
-//   configurable: true,
-//   writable: true,
-//   //   value: 'static',
-// });
-
-// console.log(obj);
-// obj.name = 20;
-
-/**
- * Example 6
- * Работа с масивом объектов
- */
-// const users = [
-//   {
-//     name: "Bob",
-//     age: 25,
-//   },
-//   {
-//     name: "Jhon",
-//     age: 20,
-//   },
-//   {
-//     name: "James",
-//     age: 30,
-//   },
-// ];
-
-// // //forEach
-// // //map
-// // //filter
-// // //sort
-// // //reduce
-// users.push({
-//   name: "Calvin",
-//   age: 50,
-// });
-
-// for (const item of users) {
-//   console.group(item.name);
-//   if (item.name === "Calvin") {
-//     item.isWork = true;
-//   }
-//   // console.log(item);
-//   for (const key in item) {
-//     console.log(key);
-//   }
-//   console.groupEnd();
-// }
-
-// console.log("This is the outer level");
-// console.group();
-// console.log("Level 2");
-// console.group();
-// console.log("Level 3");
-// console.warn("More of level 3");
-// console.groupEnd();
-// console.log("Back to level 2");
-// console.groupEnd();
-// console.log("Back to the outer level");
-// console.log(users);
-
-/**
- * Example 7
- * Всі сутності в js - це об'єкти.
- */
-// const munber2 = 10;
-// const number = new Number(10);
-
-// console.log(typeof munber2);
-// console.log(typeof number);
-
-// console.log(number);
-
-// const string1 = "Wellcome";
-// const string = new String("Wellcome");
-
-// console.log(string1.replace("W", "M"));
-// console.log(string.replace("W", "M"));
-// const array = new Array(10, 20, 30);
-
-// number.property = 5;
-// string.someValue = 'lolo';
-// array.MEGAINDEX = 10;
-
-// console.log(number);
-// console.log(string);
-// console.log(array);
-
-// console.log(number * number);
-// console.log(string.toLowerCase());
-// console.log(array.length);
-
-/**
- * Example 8
- * Власні властивості, та не власні властивості.
- */
-
-// const obj = {
-//   cat: "Catty",
-//   dog: "Doggy",
-// };
-
-// const obj2 = Object.create({});
-// obj2.slon = "Slonny";
-
-// console.log(obj2);
-
-// for (const key in obj2) {
-//   console.log(key);
-//   console.log(obj2.hasOwnProperty(key));
-// }
-// console.log(Object.keys(obj2));
-
-// ==========================================================================================
-// ==========================================================================================
-// ==========================================================================================
-
 /*
- * Операція spread как замена Object.assign({}, ...sources)
- */
-
-// const user = {
-//   firstName: "Jonathan",
-//   lastName: "Barnett",
-//   age: 30,
-// };
-
-// const user2 = { ...user };
-
-// console.log("user: ", user);
-// console.log("user2: ", user2);
-
-// user2.firstName = "Birdie";
-// user.firstName = "asdasd";
-
-// console.log("user: ", user);
-// console.log("user2: ", user2);
-
-/*
- * Розпилення складних типів. Проблема поверхносного роспилення.
- */
-
-// const user = {
-//   firstName: "Jonathan",
-//   lastName: "Barnett",
-//   age: 30,
-//   someArr: [1, 2, 3, 4],
-// };
-
-// // const user2 = { ...user };
-// const user2 = _.cloneDeep(user);
-// // const user2 = JSON.parse(JSON.stringify(user));
-
-// // console.log("user: ", user);
-// // console.log("user2: ", user2);
-
-// user2.someArr.push(10);
-
-// console.log("user: ", user);
-// console.log("user2: ", user2);
-
-/*
- * Example 2
- * Операция rest
- */
-
-/*
- * Example 2
- * Деструктурізація об'єктів
- */
-
-/**
- * Данний об'ект
- */
-
-// const firstName = "aldhjskdhgkjshdg";
-// умвно уявимо собі, що ця змінна занята десь вище по коду
-
-const user = {
-  firstName: "John",
-  lastName: "Reese",
-  age: 30,
-};
-// const { firstName: firstNameFromObject } = user;
-
-// const users = [
-//   {
-//     firstName: "John",
-//     lastName: "Reese",
-//     age: 30,
-//     isWorked: true,
-//   },
-//   {
-//     firstName: "John",
-//     lastName: "Reese",
-//     age: 30,
-//   },
-// ];
-
-// for (const item of users) {
-//   const { isWorked = false } = item;
-//   console.log(isWorked);
-// }
-
-/*
- * Напишіть деструктційне присвоення, котре:
- * для властивості firstName присвоіть до змінної firstName.
- * для властивості age присвоіть до змінної userAge.
- * для властивості isAdmin присвоіть до змінної isAdmin (false, якщо не має такого значення)
- */
-
-// let { firstName, age: userAge, isAdmin = false } = user; //можемо змынювати.
-// const { firstName, age: userAge, isAdmin = false } = user;
-// console.log(firstName, userAge, isAdmin);
-
-/*
- * Більш глибока деструктурізація об'єктів
- */
-
-// const team = {
-//   number: 4,
-//   flag: "./images/flag.jpg",
-//   employees: ["Anton", "Oleg", "Ronnie", "Carr"],
-//   langs: {
-//     original: "uk",
-//     secondary: "en",
-//   },
-// };
-
-// const { number, flag, employees, langs } = team;
-
-// console.log(langs);
-// const { original, secondary } = langs;
-// console.log(original, secondary);
-
-// const {
-//   number,
-//   flag,
-//   employees,
-//   langs: { original: originalLang, secondary: secondaryLang },
-// } = team;
-
-// console.log('number: ', number);
-// console.log('flag: ', flag);
-// console.log('employees: ', employees);
-// console.log('originalLang: ', original);
-// console.log('secondaryLang: ', secondary);
-
-/*
- * Деструктурізація масивів
+ * Example 0
+ * Імперативний та деклоративний код
+ * Імп - це послідовне виконнаня дій. Та спрощення коду до більш ранніх версій.
+ * Дек - це описання задач та використання сучастного синтаксису і патрнів.
+ * Ідеалу не існує.
  */
 
 /*
  * Example 1
- * Операция spread как замена concat и slice
+ * Колбек функції, функції вищого порядку
  */
 
-/*
- * Копія массива
- */
-// const numbers = [1, 2, 3, 4];
-// //0: 1
-// //1: 2
-// //3: 3
-
-// const numbers2 = [...numbers];
-
-// // console.log("numbers: ", numbers);
-// // console.log("numbers2: ", numbers2);
-
-// numbers2[0] = 10;
-
-// console.log("numbers: ", numbers);
-// console.log("numbers2: ", numbers2);
-
-/*
- * Об'єднання масивів
- */
-// const someNumber = 400;
-// const numbers = [1, 2, 3, 4, 5];
-// const numbers2 = [10, 9, 8, 7];
-// const allNumbers = [someNumber, ...numbers, 10, 20, ...numbers2];
-
-// console.log(allNumbers);
-
-/*
- * Розпилення масива в функцію
- */
-
-// const numbers = [1, 2, 3, 4, 5];
-// console.log(Math.max(...numbers)); //spread
-// const [first, second, third] = numbers;
-
-// function some(...args) {
-//   //rest
-//   console.log(args);
-// }
-// some(1, 2, 3, 4, 5, 6, 7);
-
-// const names = ["Herbert Todd", "Belle Soto", "Roger Marsh", "Ethan Lindsey"];
-// const [, user1, , user2] = names;
-// // const user1 = names[0];
-// // const user2 = names[2];
-// console.log(user1, user2);
-
-// const rgb = [0, 255, 34];
-// const [red, green, blue] = rgb;
-
-// console.log("red: ", red);
-// console.log("green: ", green);
-// console.log("blue: ", blue);
-
-/*
- * Напишіть функцію sum, котра суммує будь яку кілкість аргументів
- */
-// function sum(...args) {
-//   console.log(args);
-// }
-
-// function sum(...numbers) {
-//   let total = 0;
-//   for (const value of numbers) {
-//     total += value;
-//   }
-//   return total;
-// }
-
-// console.log(sum(1, 2, 3, 4, 5, 6));
-
-// const arr1 = [1, 2, 3];
-// const arr2 = [...arr1];
-// console.log(arr1 === arr2);
-
-// function foo({ username } = {}) {
-//   console.log(username);
-// }
-// foo();
-
-// const rgb = [0, 255, 34];
-// const [red, green, blue] = rgb;
-
-// const red = rgb[0];
-// const green = rgb[1];
-// const blue = rgb[2];
-
-// console.log("red: ", red);
-// console.log("green: ", green);
-// console.log("blue: ", blue);
-
-// const RGB = {
-//   red: red,
-//   green: green,
-//   blue,
+// const foo1 = function (callback) {
+//   // let callback = foo2;
+//   callback();
 // };
 
-// console.log(RGB);
+// const foo2 = function () {
+//   console.log(`Hello!`);
+// };
+
+// foo1(foo2);
+
+/**
+ *  Запит на сервер
+ **/
+// const fetchUser = async function (userUrl, callback) {
+//   const response = await fetch(`https://api.github.com/users/${userUrl}`);
+//   const result = await response.json();
+
+//   callback(result);
+// };
+
+// const showInfoAboutUser = function (user) {
+//   console.log(user);
+// };
+
+// fetchUser('marvall', showInfoAboutUser);
+
+/**
+ * Виклик калбека по евенту на сторінкі
+ * */
+// const button = document.querySelector('.button');
+
+// const logValues = function (event) {
+// //   console.log(event);
+//   //   console.log('Hi!');
+// };
+
+// button.addEventListener('click', logValues);
+// //  {
+// //     // перевіряти наш event.
+
+// //     logValues(event)
+// // }
+
+/**
+ * Виклик калбека по евенту у setTimeout
+ * */
+
+// const callback = function () {
+//   console.log('hi');
+// };
+// setTimeout(callback, 1000);
+
+// setTimeout(function () {
+//   console.log('hi');
+// }, 2000);
+
+// setTimeout(() => {
+//   console.log('hi');
+// }, 3000);
+
+/*
+ * TASK 1
+ * Напишіть функцію each(array, callback), котра першим параметром очікує масив,
+ * а другим - функцію, котра викликається для кожного едементу масива
+ * Функція each повинна повернути новий масив, елементами котрого будуть результати виклику коллбека.
+ */
+
+// const each = function (array, callback) {
+//   let newArray = [];
+
+//   for (const value of array) {
+//     newArray.push(callback(value));
+//   }
+
+//   return newArray;
+// };
+
+// const callback1 = function (item) {
+//   return item * 2;
+// };
+
+// const callback2 = function (item) {
+//   return item.toString();
+// };
+
+// const callback3 = function (item) {
+//   return item * item;
+// };
+
+// console.log(each([1, 2, 3, 4, 5], callback1));
+// console.log(each([1, 2, 3, 4, 5], callback2));
+// console.log(each([1, 2, 3, 4, 5], callback3));
+
+// const some = function Some() {
+//   console.log(arguments);
+// };
+
+// const some = function () {
+
+// }
+
+// function Some() {
+//   console.log(arguments.callee);
+// }
+
+// const some = function Some() {
+//   console.log(arguments.callee);
+// };
+
+// // const some = Some;
+
+// some();
+
+/*
+ * TASK 2
+ * Напишіть наступні функції:
+ * createProduct(product, callback) - приймає об'ект товара без id,
+ * а також колбек. Функція створює об'ект товара, додає ему унікальний
+ * id та викликає колбек передає йому новий об'ект.
+ * logProduct(product) - коллбек приймає об'ект продукта та логує його в консоль
+ * logTotalPrice(product) - коллбек приймає об'ект продукта та вbводить в консоль ціну.
+ */
+
+// console.log(Date.now()); //unix time
+// const createProduct = function (product, callback) {
+//   const newProduct = {
+//     id: Date.now(),
+//     ...product,
+//   };
+
+//   callback(newProduct);
+// };
+
+// const logProduct = function (product) {
+//   console.log(product);
+// };
+
+// const logTotalPrice = function ({ price, quantity } = {}) {
+//   console.log(`Total price: ${price * quantity}`);
+// };
+
+// createProduct({ name: '🍎', price: 30, quantity: 3 }, logProduct);
+// createProduct({ name: '🍋', price: 20, quantity: 5 }, logProduct);
+
+// createProduct({ name: '🍎', price: 30, quantity: 3 }, logTotalPrice);
+// createProduct({ name: '🍋', price: 20, quantity: 5 }, logTotalPrice);
+
+/*
+ * Example 2
+ * Стрілкові функції, явне - неявне повернення, arguments, поврнення об'екта.
+ * Круглі дужки підчас оголошння.
+ * Чому не використовуємо стрілкові функціі в методах об'екта
+ */
+
+// const some = object => ({
+//   id: Date.now(),
+//   ...object,
+// });
+
+// console.log(
+//   some({
+//     name: 'Jhon',
+//     email: 'example@domain.com',
+//   })
+// );
+
+// const object = {
+//   name: 'Jhon',
+//   sayHello() {
+//     console.log(this);
+//     console.log(`${this.name} say: Hello`);
+//   },
+// };
+// object.sayHello();
+
+/**
+ * TASK 3
+ * Переписать TASK 1 на стрілкові функції
+ */
+
+// const each = (array, callback) => {
+//   let newArray = [];
+
+//   for (const value of array) {
+//     newArray.push(callback(value));
+//   }
+
+//   return newArray;
+// };
+
+// const multiplyNumberOnTwo = number => number * 2;
+// const convertNumberToString = number => number.toString();
+// const squareOfNumber = number => number * number;
+
+// console.log(each([1, 2, 3, 4, 5], multiplyNumberOnTwo));
+// console.log(each([1, 2, 3, 4, 5], convertNumberToString));
+// console.log(each([1, 2, 3, 4, 5], squareOfNumber));
+
+/**
+ * TASK 4
+ * Переписать TASK 2 на стрілкові функції
+ */
+
+// const createProduct = (product, callback) => {
+//   //   const newProduct = {
+//   //     id: Date.now(),
+//   //     ...product,
+//   //   };
+
+//   //   callback(newProduct);
+
+//   callback({ id: Date.now(), ...product });
+// };
+
+// const createProduct = (product, callback) =>
+//   callback({ id: Date.now(), ...product });
+
+// const logProduct = product => console.log(product);
+
+// const logTotalPrice = ({ price, quantity } = {}) =>
+//   console.log(`Total price: ${price * quantity}`);
+
+// //не бажано приберати тіло стрілочної функції, якщо вони не влазить в одну строку.
+// // const logTotalPrice = ({ price, quantity } = {}) => {
+// //   console.log(`Total price: ${price * quantity}`);
+// // };
+
+// createProduct({ name: '🍎', price: 30, quantity: 3 }, logProduct);
+// createProduct({ name: '🍋', price: 20, quantity: 5 }, logProduct);
+
+// createProduct({ name: '🍎', price: 30, quantity: 3 }, logTotalPrice);
+// createProduct({ name: '🍋', price: 20, quantity: 5 }, logTotalPrice);
+
+/*
+ * Exapmle 3
+ * forEach() как замена циклов for и for...of для массивов
+ */
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// const callback = (element, index, array) => {
+//   console.log('element: ', element);
+//   console.log('index: ', index);
+//   console.log('array: ', array);
+// };
+
+// numbers.forEach(callback);
+
+// numbers.forEach((el, idx, arr) => console.log(el));
+
+// const ownForEach = function (array, callback) {
+//   for (let i = 0; i < array.length; i += 1) {
+//     callback(array[i], i, array);
+//   }
+// };
+
+// ownForEach(numbers, (el, idx, arr) => {
+//   console.log(el);
+// });
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// const getTotal = function (numbersArr) {
+//   let total = 0;
+
+//   // Через обычный for
+//   //   for (let i = 0; i < numbersArr.length; i += 1) {
+//   //     total += numbersArr[i];
+//   //   }
+
+//   // Через обычный for...of
+//   for (const num of numbersArr) {
+//     total += num;
+//   }
+
+//   // Через forEach();
+//   numbersArr.forEach(el => (total += el));
+
+//   return total;
+// };
+
+// console.log(getTotal(numbers));
+
+/*
+ * є функція, зробити рефакторіг, переписати її на forEach.
+ */
+
+// function logItems(items) {
+//   console.log(items);
+
+//   for (let i = 0; i < items.length; i += 1) {
+//     console.log(`${i + 1} - ${items[i]}`);
+//   }
+// }
+
+// const logItems = items =>
+//   items.forEach((item, index) => console.log(`${index + 1} - ${item}`));
+
+// const logItems = items => {
+//   items.forEach((item, index) => {
+//     console.log(`${index + 1} - ${item}`);
+//   });
+// };
+
+// logItems(['Mango', 'Poly', 'Ajax']);
+// logItems(['🍎', '🍇', '🍑', '🍌', '🍋']);
+
+/*
+ * є функція, зробити рефакторіг, переписати її на forEach та стрілкові функції.
+ */
+
+// function printContactsInfo({ names, phones } = {}) {
+//   const nameList = names ? names.split(',') : [];
+//   const phoneList = phones ? phones.split(',') : [];
+
+//   for (let i = 0; i < nameList.length; i += 1) {
+//     console.log(`${nameList[i]}: ${phoneList[i]}`);
+//   }
+// }
+
+// const printContactsInfo = ({ names, phones } = {}) => {
+//   const nameList = names ? names.split(',') : [];
+//   const phoneList = phones ? phones.split(',') : [];
+
+//   nameList.forEach((name, index) => {
+//     console.log(`${name}: ${phoneList[index]}`);
+//   });
+// };
+
+// printContactsInfo({
+//   names: 'Jacob,William,Solomon,Artemis',
+//   phones: '89001234567,89001112233,890055566377,890055566300',
+// });
+
+/*
+ * є функція, зробити рефакторіг, переписати її на forEach.
+ */
+
+// function calсulateAverage(...args) {
+//   let total = 0;
+
+//   for (let i = 0; i < args.length; i++) {
+//     total += args[i];
+//   }
+
+//   return total / args.length;
+// }
+
+// const calсulateAverage = (...numbers) => {
+//   let total = 0;
+
+//   numbers.forEach(number => (total += number));
+
+//   return total / numbers.length;
+// };
+
+// console.log(calсulateAverage(1, 2, 3, 4)); // 2.5
+// console.log(calсulateAverage(14, 8, 2)); // 8
+// console.log(calсulateAverage(27, 43, 2, 8, 36)); // 23.2
