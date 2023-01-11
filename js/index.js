af9a145fd0d2dabefcac7a46ebe362c42035b057
marvall (Yaroslav Bezugliy) <marvallua@gmail.com>
2023-01-11 23:20:58 +0200

    init

--- js/index.js
+++ js/index.js
@@ -1,245 +1,195 @@
 'use strict';
-//* Об'єкти window і document
-// console.log(window);
-// console.dir(document);
-// console.log(location);
-// console.log(navigator);
-
-//* Cтарі методи пошуку елементів (getElementBy...)
-
-// const titleElement = document.getElementById('title');
-// const listItemElement = document.getElementsByClassName('base__image');
-
-// console.dir(titleElement);
-// console.dir(listItemElement);
-// * Сучасні методи пошуку елементів (querySelector, querySelectorAll)
-
-// const ref = {
-//   mainTitleElement: document.querySelector('#title'),
-//   listItemElements: document.querySelectorAll('.list__item'),
-// };
-
-// const mainTitleElement = document.querySelector('#title');
-// const listItemElements = document.querySelectorAll('.list__item');
-
-// console.dir(mainTitleElement);
-// console.dir(listItemElements);
-
-//* Навігації по DOM дереву
-// elem.parentNode - вибере батьківський elem.
-// elem.childNodes - псевдомасив, зберігає всі дочірні елементи, включно з текстовими.
-// elem.children - псевдомасив, зберігає тільки дочірні вузли-елементи, тобто ті, що відповідають тегам.
-// elem.firstChild - вибере перший дочірній елемент всередині elem, включно з текстовими вузлами.
-// elem.firstElementChild - вибере перший дочірній вузол-елемент всередині elem.
-// elem.lastChild - вибере останній дочірній елемент всередині elem, включно з текстовими вузлами.
-// elem.lastElementChild - вибере останній дочірній вузол-елемент всередині elem.
-// elem.previousSibling - вибере елемент «зліва» від elem (його попереднього сусіда).
-// elem.previousElementSibling - вибере вузол-елемент «зліва» від elem (його попереднього сусіда).
-// elem.nextSibling - вибере елемент «праворуч» від elem (його наступного сусіда)
-// elem.nextElementSibling - вибере вузол-елемент «праворуч» від elem (його наступного сусіда).
-
-// const listElement = document.querySelector('.list');
-// console.dir(listElement.nextElementSibling);
-
 /**
- * Атрибути як властивості.
+ *  ================ Example 1 ==================
+ * Поширення подій. Спливання подій. stopPropogation
+ * Event.target ( на якому відбулась подія )
+ * Event.currentTarget ( на якому знаходиться лістенер )
+ * параметри AddEventListeners(event, callback, options, capture)
  */
 
-// const imageElement = document.querySelector('#baseImage');
-// console.dir(imageElement);
-// imageElement.src = 'https://picsum.photos/id/6/200/300';
-// imageElement.title = 'notebook';
-// imageElement.alt = 'notebook';
-// imageElement.width = 300;
+// const section = document.querySelector('.section');
+// // const div = document.querySelector('.div');
+// // const p = document.querySelector('.description');
+// const button = document.querySelector('.action');
 
-// https://picsum.photos/200/300
-// const actionButtonElement = document.querySelector('.action');
-// actionButtonElement.addEventListener('click', () => {
-//   imageElement.src = `https://picsum.photos/id/${Math.floor(Math.random() * 1000)}/200/300`;
-//   imageElement.alt = 'random image';
+// const eventHandler = function (event) {
+//   event.stopPropagation();
+//   // console.dir(event.currentTarget);
+//   // console.dir(event.target);
+//   console.log(event.currentTarget.nodeName);
+// };
+
+// const eventHandler = function (event) {
+//   if (event.target.nodeName !== 'BUTTON') {
+//     console.log('We take section');
+//   }
+// };
+
+// const eventHandler1 = function (event) {
+//   if (event.target === event.currentTarget) {
+//     console.log('We take button');
+//   }
+// };
+
+// section.addEventListener('click', eventHandler1);
+// // div.addEventListener('click', eventHandler);
+// // p.addEventListener('click', eventHandler);
+// button.addEventListener('click', eventHandler1);
+
+/*
+ * параметри addEventListener(event, callback, options)
+ *
+ */
+
+// passive
+// const form = document.querySelector('.form');
+// const eventHandler = function (event) {
+//   event.preventDefault();
+//   console.log(event);
+// };
+
+// form.addEventListener(
+//   'submit',
+//   eventHandler,
+//   {
+//     // capture: true,
+//     // once: true,
+//     passive: true,
+//   },
+// );
+
+// const section = document.querySelector('.section');
+// const div = document.querySelector('.div');
+// const p = document.querySelector('.description');
+// const button = document.querySelector('.action');
+
+// const eventHandler = function (event) {
+//   console.log(event.currentTarget.nodeName);
+// };
+
+// section.addEventListener('click', eventHandler, {
+//   capture: true,
+//   // once: true,
+//   //   passive: true,
+// });
+// div.addEventListener('click', eventHandler, {
+//   capture: true,
+//   // once: true,
+//   //   passive: true,
+// });
+// p.addEventListener('click', eventHandler, {
+//   capture: true,
+//   // once: true,
+//   //   passive: true,
+// });
+// button.addEventListener('click', eventHandler, {
+//   capture: true,
+//   // once: true,
+//   //   passive: true,
 // });
 
-/*
- * Доступ до атрибутів
- *
- * elem.hasAttribute(name)
- * elem.getAttribute(name)
- * elem.setAttribute(name, value)
- * elem.removeAttribute(name)
- * elem.attributes
- */
-
-// const imageElement = document.querySelector('#baseImage');
-// console.log(imageElement.removeAttribute('class'));
-// console.log(imageElement.hasAttribute('src'));
-// console.log(imageElement.getAttribute('class'));
-//imageElement.setAttribute('src', `https://picsum.photos/id/${Math.floor(Math.random() * 1000)}/200/300`);
-
 /**
- * Data Atributes
+ *  ================ Example 2 ==================
+ * Делегування подій
+ * Написати генерацію календаря.
+ * Написати делегування подій для календаря, таким чином, щом при клікі на день, він позначався як віделиний.
+ * Якщо ми будему клікати на день тиждня, то будуть виділятись всі дні цього тижня.
  */
 
-// const imageElement = document.querySelector('#baseImage');
-// imageElement.dataset.index = 'IMAGE';
-// console.dir(imageElement);
+const calendar = document.querySelector('.calendar');
 
-/**
- *  Об'єкт style, cssText
- */
-// const titleElement = document.querySelector('#title');
-// console.dir(titleElement.style);
-// titleElement.style.color = '#777777';
-// titleElement.style.fontSize = '50px';
-// titleElement.style.backgroundColor = '#caffdd';
-// titleElement.style.padding = '20px';
-
-// titleElement.style.cssText = `
-//     color: #777777;
-//     font-size: 30px;
-//     background-color: #caffdd;
-//     padding: 50px;
-//   `;
-// titleElement.style = `
-//     color: #777777;
-//     font-size: 30px;
-//     background-color: #caffdd;
-//     padding: 20px;
-// `;
-
-// console.log(titleElement);
-
-//* Методи об'єкту classList (add, remove, toggle, contains)
-
-// const rootElement = document.querySelector('.root');
-// console.log(rootElement.classList);
-// // rootElement.classList.add('red');
-// // rootElement.classList.remove('red');
-// // console.log(rootElement.classList.contains('red'));
-// // console.log(rootElement.className);
-// rootElement.classList.toggle('green');
-
-// const modalElement = document.querySelector('.modal');
-// const openModalButton = document.querySelector('.openModal');
-// const closeModalButton = document.querySelector('.closeModal');
-
-// const callback = () => {
-//   modalElement.classList.toggle('hidden');
-// };
-
-// openModalButton.addEventListener('click', callback);
-// closeModalButton.addEventListener('click', callback);
-
-/**
- * Card of Gallery
- */
-
-/*
-<li class="gallery-item">
-  <a href="#">
-    <img src="https://picsum.photos/id/237/200/300" alt="Labrador">
-    <p>Some Description</p>
-  </a>
-</li>
-*/
-
-// const galleryRoot = document.querySelector('#rootList');
-
-// const makeGalleryItem = function () {
-//   const li = document.createElement('li');
-//   li.classList.add('gallery-item');
-
-//   const a = document.createElement('a');
-//   a.href = '#';
-//   li.append(a);
-
-//   const img = document.createElement('img');
-//   img.src = 'https://picsum.photos/id/237/200/300';
-//   img.alt = 'Labrador';
-
-//   const p = document.createElement('p');
-//   p.textContent = 'Some Description';
-//   a.append(img, p);
-
-//   return li;
-// };
-
-// galleryRoot.append(makeGalleryItem());
-
-/**
- * Створити галерею на основі масиву pictures
- */
-
-/*
-  <li class="gallery-item">
-    <a href="#">
-      <img src="https://picsum.photos/id/237/200/300" alt="Labrador" width="" height="">
-      <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non laudantium nostrum, aliquid impedit sit repellendus neque minima
-            repudiandae nulla sed dolorem dolore corporis consectetur quam magni
-            vel numquam. Ipsam, ut?
-      </p>
-    </a>
-  </li>
-  */
-
-// Створити карточку на основі document.createElement
-// Перебір масиву pictures через map
-// Вставка колекції карток на сторінку за допомогою append(...args);
-
-//pictures
-// const galleryRoot = document.querySelector('#rootList');
-// const makeGalleryItem = function ({ classList, width, alt, description, height, url }) {
-//   const li = document.createElement('li');
-//   li.classList.add(classList);
-
-//   const a = document.createElement('a');
-//   a.href = '#';
-//   li.append(a);
-
-//   const img = document.createElement('img');
-//   img.src = url;
-//   img.alt = alt;
-//   img.width = width;
-//   img.height = height;
-
-//   const p = document.createElement('p');
-//   p.textContent = description;
-
-//   a.append(img, p);
-//   return li;
-// };
-
-// const elements = pictures.map((el, index, array) => makeGalleryItem(el));
-// const elements = pictures.map(makeGalleryItem);
-// galleryRoot.append(...pictures.map(makeGalleryItem));
-
-// Створити карточку на основі шаблонної строки
-// Вставка колекції карток на сторінку за допомогою innerHTML;
-// Вставка колекції карток на сторінку за допомогою insertAdjacentHTML('afterbegin', [array].join(''));
-
-// console.log(pictures);
-
-const galleryRoot = document.querySelector('#rootList');
-
-const makeGalleryItem = function (picture) {
-  return `
-  <li class="${picture.classList}">
-    <a href="#">
-      <img
-        src="${picture.url}"
-        alt="${picture.alt}"
-        width="${picture.width}"
-        height="${picture.height}"
-      >
-      <p>${picture.description}</p>
-    </a>
-  </li>
-  `;
+const makeCalendarObjects = function () {
+  const startOfMonth = moment().startOf('month');
+  const daysOfMonth = moment().daysInMonth();
+  const daysCount = daysOfMonth / 7 > 4 ? 35 : 28;
+  const weeks = [];
+  let week = [];
+  for (let i = 1; i <= daysCount; i += 1) {
+    if (startOfMonth.days() > i) {
+      week.push({ index: i, day: null });
+    } else {
+      week.push({
+        index: i,
+        day: startOfMonth.month() === moment().month() ? startOfMonth.date() : null,
+      });
+      startOfMonth.add(1, 'd');
+    }
+    if (week.length === 7) {
+      weeks.push([...week]);
+      week = [];
+    }
+  }
+  return weeks;
 };
-// console.log(galleryRoot.innerHTML);
-const elements = pictures.map((el, index, array) => makeGalleryItem(el));
 
-//innerHTML перезатре галерею.
-// galleryRoot.innerHTML = elements;
+const makeCalendarMarkUp = function (calendarObjects) {
+  let calendar = calendarObjects
+    .map(week => {
+      return `<tr>\n
+        ${week
+          .map(({ day }, index) => {
+            return `
+            <td
+              class="table-day"
+              data-day="${day ? day : ''}"
+              data-week="${index + 1}" >${day ? day : ''}
+            </td>\n`;
+          })
+          .join('')}
+      </tr>\n`;
+    })
+    .join('');
+  return `
+    <table>
+        <thead>
+          <tr>
+              <th class="table-day" data-week="1" data-main="true">Пнд</th>
+              <th class="table-day" data-week="2" data-main="true">Вт</th>
+              <th class="table-day" data-week="3" data-main="true">Ср</th>
+              <th class="table-day" data-week="4" data-main="true">Чт</th>
+              <th class="table-day" data-week="5" data-main="true">Пт</th>
+              <th class="table-day" data-week="6" data-main="true">Сбт</th>
+              <th class="table-day" data-week="7" data-main="true">Нд</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${calendar}
+        </tbody>
+    </table>
+    `;
+};
 
-galleryRoot.insertAdjacentHTML('beforeend', elements.join(''));
+const root = document.querySelector('.calendar');
+root.insertAdjacentHTML('afterbegin', makeCalendarMarkUp(makeCalendarObjects()));
+const handler = event => {
+  const tds = document.querySelectorAll('[data-day]');
+  if (event.target.dataset.main) {
+    tds.forEach(item => {
+      if (item.dataset.week === event.target.dataset.week) {
+        if (item.dataset.day) {
+          item.classList.add('selected');
+        }
+      } else {
+        item.classList.remove('selected');
+      }
+    });
+  } else if (event.target.dataset.day) {
+    tds.forEach(item => {
+      if (item === event.target) {
+        item.classList.add('selected');
+      } else {
+        item.classList.remove('selected');
+      }
+    });
+  }
+  const customEvent = new Event('enter', {
+    type: 'enter',
+  });
+  calendar.dispatchEvent(customEvent);
+};
+
+root.addEventListener('click', handler);
+
+calendar.addEventListener('enter', e => {
+  console.log(e.type);
+});
